b22108158d46ce233f6706b701b8c63ca69c9757
Fabio <fabiodn.798@gmail.com>
2023-10-01 19:15:48 +0200

    Initial commit

---
+++ src/app/interfaces/interfaces.ts
@@ -0,0 +1,19 @@
+export interface LoginUser {
+  email: string;
+  password: string;
+}
+
+export interface RegisterUser extends LoginUser {
+  name: string;
+  img: string;
+}
+
+export interface User extends RegisterUser {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  friends: string[];
+  enemies: string[];
+  img: string;
+}
