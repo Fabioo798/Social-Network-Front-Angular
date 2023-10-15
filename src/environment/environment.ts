fd7e243e05e093a5d4d64d9ee3107f0caf1d26fa
Fabio <fabiodn.798@gmail.com>
2023-10-15 16:03:25 +0200

    🤡 Create environment file

---
+++ src/environment/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+ production: false,
+ urlBasic: 'http://localhost:5000/users/',
+ urlRegister: 'http://localhost:5000/users/register',
+ urlLogin: 'http://localhost:5000/users/login',
+ urlAddRelation: 'http://localhost:5000/users/add_relation/',
+ urlRemoveRelation: 'http://localhost:5000/users/remove_relation/',
+ urlEditUser: 'http://localhost:5000/users/edit_profile/',
+};
