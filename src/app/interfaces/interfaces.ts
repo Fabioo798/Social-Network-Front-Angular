9d5df98aad7f232ab8086299f07d8a0ecf4d4b90
Fabio <fabiodn.798@gmail.com>
2023-10-15 15:52:16 +0200

    🚩 Update interfaces

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -1,19 +1,40 @@
 export interface LoginUser {
-  email: string;
-  password: string;
+ email: string;
+ password: string;
 }
 
 export interface RegisterUser extends LoginUser {
-  name: string;
-  img: string;
+ name: string;
+ img?: string;
 }
 
 export interface User extends RegisterUser {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  friends: string[];
-  enemies: string[];
-  img: string;
+ id: string;
+ name: string;
+ email: string;
+ password: string;
+ friends: string[];
+ enemies: string[];
+ img: string;
+}
+
+export interface ApiLoginResponse {
+ results: {
+  token: string;
+  data: User;
+ };
+}
+
+export interface apiRegisterResponse {
+ message: string;
+}
+
+export interface apiUpdateResponse {
+ results: [User];
+}
+
+export interface apiGetAllUsersResponse {
+ results: {
+  data: User[];
+ };
 }
