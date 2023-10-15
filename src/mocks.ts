31ed158ba49f777e7afdd6ef90ad87b65f55149d
Fabio <fabiodn.798@gmail.com>
2023-10-15 15:51:43 +0200

    🔨 Create mock file for testing

---
+++ src/mocks.ts
@@ -0,0 +1,24 @@
+import { LoginUser, RegisterUser, User } from './app/interfaces/interfaces';
+
+export const mockRegisterUser: RegisterUser = {
+ name: 'test',
+ email: 'test',
+ password: 'test',
+};
+
+export const mockLoginUser: LoginUser = {
+ email: 'test',
+ password: 'test',
+};
+
+export const mockUser: Partial<User> = {
+ id: '2013',
+ name: 'test',
+ email: 'test',
+ friends: [],
+ enemies: [],
+}
+
+export const mockToken: string = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
+  .eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ
+  .XbPfbIHMI6arZ3Y922BhjWgQzWXcXNrz0ogtVhfEd2o`;
